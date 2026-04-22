e12f68b9a43f11badec06bb675d10984ccc4b14e
Avik Kundu <kunduavik155@gmail.com>
2026-04-22 23:05:28 +0530

    some updates

--- demo /demo/src/main/java/internship/demo/config/SecurityConfig.java
+++ demo /demo/src/main/java/internship/demo/config/SecurityConfig.java
@@ -1,13 +1,24 @@
 package internship.demo.config;
 
-
 import org.springframework.context.annotation.*;
-import org.springframework.security.crypto.bcrypt.*;
-
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/**").permitAll() // allow register/login
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
